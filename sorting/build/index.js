427218fb918a78ff4a73388a17cbdb2e8b4bd87c
akshay-sahu-dev <akki5233@gmail.com>
2021-07-26 12:28:24 +0530

    Sorting Algo for strings

--- sorting/build/index.js
+++ sorting/build/index.js
@@ -1,8 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+var CharactersCollection_1 = require("./CharactersCollection");
 var NumbersCollection_1 = require("./NumbersCollection");
 var sorter_1 = require("./sorter");
 var numbersCollection = new NumbersCollection_1.NumbersCollection([10, -4, 3, 77, 45, 2]);
-var sorter = new sorter_1.Sorter(numbersCollection);
+var charactersCollection = new CharactersCollection_1.CharactersCollection('yuAhjdAa');
+// const sorter = new Sorter(numbersCollection);
+// sorter.sort();
+// console.log(numbersCollection.data);
+var sorter = new sorter_1.Sorter(charactersCollection);
 sorter.sort();
-console.log(numbersCollection.data);
+console.log(charactersCollection.data);
--- sorting/src/index.ts
+++ sorting/src/index.ts
@@ -1,9 +1,16 @@
+import { CharactersCollection } from './CharactersCollection';
 import { NumbersCollection } from './NumbersCollection';
 import { Sorter } from './sorter';
 
 const numbersCollection = new NumbersCollection([10, -4, 3, 77, 45, 2]);
+const charactersCollection = new CharactersCollection('yuAhjdAa');
 
-const sorter = new Sorter(numbersCollection);
+// const sorter = new Sorter(numbersCollection);
+// sorter.sort();
+
+// console.log(numbersCollection.data);
+
+const sorter = new Sorter(charactersCollection);
 sorter.sort();
 
-console.log(numbersCollection.data);
+console.log(charactersCollection.data);
