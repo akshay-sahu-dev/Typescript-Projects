2a8a44123f093b8e6bbe09e09fc8f9f490db0981
akshay-sahu-dev <akki5233@gmail.com>
2021-09-13 16:43:40 +0530

    Added sorting implementation for linked list

--- sorting/src/index.ts
+++ sorting/src/index.ts
@@ -1,16 +1,29 @@
 import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 import { NumbersCollection } from './NumbersCollection';
 import { Sorter } from './sorter';
 
 const numbersCollection = new NumbersCollection([10, -4, 3, 77, 45, 2]);
 const charactersCollection = new CharactersCollection('yuAhjdAa');
 
+const linkedlist = new LinkedList();
+
+linkedlist.add(4);
+linkedlist.add(2);
+linkedlist.add(49);
+linkedlist.add(12);
+linkedlist.add(-3);
+
+const sorter = new Sorter(linkedlist);
+sorter.sort();
+linkedlist.print();
+
 // const sorter = new Sorter(numbersCollection);
 // sorter.sort();
 
 // console.log(numbersCollection.data);
 
-const sorter = new Sorter(charactersCollection);
-sorter.sort();
+// const sorter = new Sorter(charactersCollection);
+// sorter.sort();
 
-console.log(charactersCollection.data);
+// console.log(charactersCollection.data);
--- sorting/src/sorter.ts
+++ sorting/src/sorter.ts
@@ -4,17 +4,15 @@ interface Sortable {
   swap(leftIndex: number, rightIndex: number): void;
 }
 
-export abstract class Sorter {
-  abstract compare(leftIndex: number, rightIndex: number): boolean;
-  abstract swap(leftIndex: number, rightIndex: number): void;
-  abstract length: number;
+export class Sorter {
+  constructor(public collection: Sortable) {}
 
   sort(): void {
-    const { length } = this;
+    const { length } = this.collection;
     for (let i = 0; i < length; i++) {
       for (let j = 0; j < length - i - 1; j++) {
-        if (this.compare(j, j + 1)) {
-          this.swap(j, j + 1);
+        if (this.collection.compare(j, j + 1)) {
+          this.collection.swap(j, j + 1);
         }
       }
     }
